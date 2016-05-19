955a7f468a2bf328c3108ae6b550fa936cb7ca1a
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-05-19 16:52:29 -0400

    Logic testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,10 +12,14 @@ $("#submitName1").on("click", function() {
 		name: name
 	})
 
+	playerNum = 1;
+
 	$("#nameinput1").addClass("hide");
 	$("#nameinput2").removeClass("hide");
 	$("#submitName1").addClass("hide");
 	$("#submitName2").removeClass("hide");
+
+	playGame();
 
 	return false;
 });
@@ -28,6 +32,14 @@ $("#submitName2").on("click", function() {
 		name2: name2
 	})
 
+	playerNum = 2;
+
+	$("#submitName2").addClass("hide");
+	$("#nameinput2").addClass("hide");
+	$(".nameinputlabel").addClass("hide");
+
+	playGame();
+
 	return false;
 });
 
@@ -39,3 +51,33 @@ rps.on("child_added", function(snapshot) {
 	$(".p2name").html(snapshot.val().name2);
 
 });
+
+
+
+function playGame(){
+
+	var cTitleDiv = ("<div class='cTitle'>" + "Please make a choice:" + "</div>");
+	var rockDiv = ("<div class='rock'>"  + "Rock" + "</div>");
+	var paperDiv = ("<div class='paper'>"  + "Paper" + "</div>");
+	var scissorsDiv = ("<div class='scissors'>" + "Scissors" + "</div>");
+
+	if (playerNum == 1){
+		$(".user1picks").append(cTitleDiv);
+		$(".user1picks").append(rockDiv);
+		$(".user1picks").append(paperDiv);
+		$(".user1picks").append(scissorsDiv);
+
+		//$(".user2picks").hide();
+	}
+	else if (playerNum == 2){
+		$(".user2picks").append(cTitleDiv);
+		$(".user2picks").append(rockDiv);
+		$(".user2picks").append(paperDiv);
+		$(".user2picks").append(scissorsDiv);
+
+		$(".user1picks").hide();
+	}
+
+}
+
+
